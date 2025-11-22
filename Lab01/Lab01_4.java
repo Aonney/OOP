d734a1467ed8b771c457c4cd5c49fb88459cc9b8
Santi Panyanoi <aon007567@gmail.com>
2025-11-22 20:27:49 +0700

    Create Lab01_4.java

---
+++ Lab01/Lab01_4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Lab01_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter score (0-100): ");
+        int score = sc.nextInt();
+
+        if (score >= 80 && score <= 100) {
+            System.out.println("Grade: A");
+        } else if (score >= 70) {
+            System.out.println("Grade: B");
+        } else if (score >= 60) {
+            System.out.println("Grade: C");
+        } else if (score >= 50) {
+            System.out.println("Grade: D");
+        } else if (score >= 0) {
+            System.out.println("Grade: F");
+        } else {
+            System.out.println("Invalid score!");
+        }
+    }
+}
