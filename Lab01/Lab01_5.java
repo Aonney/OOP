46aeab033f1dd20def8878e34ff2177b737e12bd
Santi Panyanoi <aon007567@gmail.com>
2025-11-22 20:33:09 +0700

    Create Lab01_5.java

---
+++ Lab01/Lab01_5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Lab01_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter menu number (1-4): ");
+        int menu = sc.nextInt();
+
+        if (menu == 1) {
+            System.out.println("Americano");
+        } else if (menu == 2) {
+            System.out.println("Latte");
+        } else if (menu == 3) {
+            System.out.println("Espresso");
+        } else if (menu == 4) {
+            System.out.println("Mocha");
+        } else {
+            System.out.println("Invalid Menu");
+        }
+    }
+}
