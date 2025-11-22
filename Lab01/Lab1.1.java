4a7162c2acd62d60b736adcda2e2cd78273e17b3
Santi Panyanoi <aon007567@gmail.com>
2025-11-22 19:56:57 +0700

    Update Lab1.1.java

--- Lab01/Lab1.1.java
+++ Lab01/Lab1.1.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // รับค่าจำนวนเต็มตัวแรก
+        System.out.print("Enter first integer: ");
+        int num1 = sc.nextInt();
+
+        // รับค่าจำนวนเต็มตัวที่สอง
+        System.out.print("Enter second integer: ");
+        int num2 = sc.nextInt();
+
+        // คำนวณผลบวก
+        int sum = num1 + num2;
+
+        // แสดงผลลัพธ์
+        System.out.println("Sum = " + sum);
+    }
+}
 
