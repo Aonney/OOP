42e8aac8f7b99e744617e813268adc5e5546af0c
Santi Panyanoi <aon007567@gmail.com>
2025-11-22 20:55:24 +0700

    Create Lab01_8.java

---
+++ Lab01/Lab01_8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Lab01_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int N = sc.nextInt();
+
+        int[] numbers = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        int max = numbers[0];
+        for (int i = 1; i < N; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Maximum Value = " + max);
+    }
+}
