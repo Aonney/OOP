5878298ecee09fd88c8a1156f8f83bfe35032aa9
Santi Panyanoi <aon007567@gmail.com>
2025-11-22 20:22:12 +0700

    Create Lab01.3.java

---
+++ Lab01/Lab01.3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Lab01_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = sc.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
