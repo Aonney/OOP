8ab857e29f69cc3c98193106415b7a316ff23075
Santi Panyanoi <aon007567@gmail.com>
2025-11-29 21:18:49 +0700

    Update Lab02_2.java

--- Lab02/Lab02_2.java
+++ Lab02/Lab02_2.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
 
+class Student {
+
+    String studentId;  // Attribute 1
+    String name;       // Attribute 2
+
+    // Method แสดงผลข้อมูลในอ็อบเจกต์
+    void displayInfo() {
+        System.out.println(studentId);
+        System.out.println(name);
+    }
+}
+
+public class Lab02_2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Student ID");
+        System.out.println("Full Name");
+
+        // รับข้อมูล
+        String studentId = scanner.nextLine();
+        String name = scanner.nextLine();
+
+        // สร้างอ็อบเจกต์ Student
+        Student student = new Student();
+        student.studentId = studentId;
+        student.name = name;
+
+        // เรียกใช้เมธอด displayInfo()
+        student.displayInfo();
+
+        scanner.close();
+    }
+}
