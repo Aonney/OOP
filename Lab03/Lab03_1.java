84545ef6e76469c532bb01c02c09fb716a68860f
Santi Panyanoi <aon007567@gmail.com>
2025-12-14 16:58:02 +0700

    Update Lab03_1.java

--- Lab03/Lab03_1.java
+++ Lab03/Lab03_1.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+
+class User {
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
+
+public class Lab03_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        User user = new User(name);
+
+        System.out.println(user.getUsername());
+    }
+}
 
